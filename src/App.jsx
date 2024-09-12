d8d4ddb7106ba0a60a86eb3f89cec4d041b98183
ryanmody <rahimmody488@gmail.com>
2024-09-11 21:45:41 -0400

    Initial commit + BrowserRouter add + env file add

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+
+      <Routes>
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>  
+    
+    </BrowserRouter>
+
+
+    </>
+  )
+}
+
+export default App
