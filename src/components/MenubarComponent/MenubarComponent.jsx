5b0d068733218d2df7e419a83e0daca15e4a734c
ryanmody <rahimmody488@gmail.com>
2024-09-12 22:27:58 -0400

    Minor changes to text content of menu bar

--- src/components/MenubarComponent/MenubarComponent.jsx
+++ src/components/MenubarComponent/MenubarComponent.jsx
@@ -3,20 +3,24 @@ import "./MenubarComponent.scss";
 
 const MenubarComponent = () => {
   return (
-    <div className="menu-bar">
-      <div className="menu-bar__container">
-        <div className="menu-bar__item menu-bar__stack"></div>
-        <div className="menu-bar__item--bold">All</div>
+      <div className="menu-bar__box">
+        <div className="menu-bar">
+          <div className="menu-bar__container">
+            <div className="menu-bar__item menu-bar__stack"></div>
+            <div className="menu-bar__item--bold">All</div>
+          </div>
+          <div className="menu-bar__item">Fashion</div>
+          <div className="menu-bar__item">Mobiles</div>
+          <div className="menu-bar__item">Gift Ideas</div>
+          <div className="menu-bar__item">Prime</div>
+          <div className="menu-bar__item">Amazon Pay</div>
+          <div className="menu-bar__item">Gift Cards</div>
+          <div className="menu-bar__item">Sports, Fitness & Outdoors</div>
+          <div className="menu-bar__item">Computers</div>
+          <div className="menu-bar__item">Customer Service</div>
+        </div>
+        <div className="menu-bar__image"></div>
       </div>
-      <div className="menu-bar__item">Deals Store</div>
-      <div className="menu-bar__item">Prime</div>
-      <div className="menu-bar__item">Buy Again</div>
-      <div className="menu-bar__item">Registry</div>
-      <div className="menu-bar__item">Browsing History</div>
-      <div className="menu-bar__item">Ryan's Store</div>
-      <div className="menu-bar__item">Best Sellers</div>
-      <div className="menu-bar__item">Home</div>
-    </div>
   );
 };
 
