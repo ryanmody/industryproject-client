ab486f992a73c02991bbd55a82b232628c4264af
哇哈哈哈 <wahahaha@MacBook-Pro.local>
2024-09-13 00:44:03 -0400

    add footer component

--- src/pages/ReviewPurchasePage.jsx
+++ src/pages/ReviewPurchasePage.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import MenubarComponent from "../components/MenubarComponent/MenubarComponent";
 import HeaderComponent from "../components/HeaderComponent/HeaderComponent";
+import FooterComponent from "../components/FooterComponent/FooterComponent";
 
 const ReviewPurchasePage = () => {
   return (
     <>
-      <HeaderComponent />
-      <MenubarComponent />
+      <header>
+        <HeaderComponent />
+        <MenubarComponent />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <FooterComponent />
+      </footer>
+
     </>
   );
 };
